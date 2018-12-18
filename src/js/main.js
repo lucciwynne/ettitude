a3343477ffc6d5033164055ebc3cf280b3069162
paulacaixeta <vivaluccistudio@gmail.com>
2018-12-18 12:11:30 -0800

    Added function to close menu when clicking away

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,15 @@
 // CSS imports
 import '../sass/main.scss';
+
+// Close navigation menu when clicking off
+const elements = {
+    checkbox: document.querySelector('.navigation__check'),
+    menu: document.querySelector('.navigation__btn'),
+    icon: document.querySelector('.navigation__btn--icon')
+};
+
+document.addEventListener('click', e => {
+    if (e.target !== elements.checkbox && e.target !== elements.icon && e.target !== elements.menu) {
+      elements.checkbox.checked = false;
+    }
+});
